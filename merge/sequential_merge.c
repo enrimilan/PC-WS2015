1c9058abd74ed07989f03504caac84f12a58f5af
enrimilan <enrimiho@gmail.com>
2015-12-07 12:40:27 +0100

    added a reference implementation of mergesort while listening to years & years - king

--- merge/sequential_merge.c
+++ merge/sequential_merge.c
@@ -7,8 +7,35 @@ double sequential_merge(data_t *A, int A_len, data_t *B, int B_len, data_t *resu
 
 	usecs start = getTimestamp();
 
-	// TODO: implement!
+	int i = 0;
+	int j = 0;
+	int k = 0;
+
+	while(i<A_len && j<B_len){
+		if(A[i]<B[j]){
+			result[k] = A[i];
+			i++;
+		}
+		else{
+			result[k] = B[j];
+			j++;
+		}
+		k++;
+	}
+
+	while(i<A_len){
+		result[k] = A[i];
+		i++;
+		k++;
+	}
+
+	while(j<B_len){
+		result[k] = B[j];
+		j++;
+		k++;
+	}
 	
 	usecs end = getTimestamp();
+	
 	return timeDiff(start, end);
 }
