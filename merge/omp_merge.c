f84728eb28e20c237978f4318dee1af56ec3a777
enrimilan <enrimiho@gmail.com>
2015-12-22 17:36:52 +0100

    some more speedup when using the parallel constructs of openmp

--- merge/omp_merge.c
+++ merge/omp_merge.c
@@ -2,46 +2,26 @@
 #include "sequential_merge.h"
 #include "omp.h"
 
-/** Prototypes */
-
-void omp_merge_part(int start, int end, data_t *A, int lenA, data_t *B, int lenB, data_t *result);
-
-/** Implementations */
-
 double omp_merge(data_t *A, int lenA, data_t *B, int lenB, data_t *result) {
 	if (result == NULL)
 		return 0;
 
+	//usually some multiple of the number of cores	
+	int p = omp_get_max_threads();
 	double start = getTimestamp();
-	omp_merge_part(0, lenA+lenB, A, lenA, B, lenB, result);
+	#pragma omp parallel
+	{
+ 		#pragma omp for
+ 		for (int i=0; i<p; i++) {
+ 			int startA, endA, startB, endB;
+ 			int start = i*(lenA+lenB)/p;
+ 			int end = (i+1)*(lenA+lenB)/p;
+			coranks(start, A, lenA, &startA, B, lenB, &startB);
+			coranks(end, A, lenA, &endA, B, lenB, &endB);
+			(void) sequential_merge(A+startA, endA-startA, B+startB, endB-startB, result+start);
+ 		}
+	}
 	double end = getTimestamp();
 	
 	return end-start;
-}
-
-void omp_merge_part(int start, int end, data_t *A, int lenA, data_t *B, int lenB, data_t *result){
-
-	//get number of available threads
-	int n = omp_get_max_threads() - omp_get_num_threads();
-	if(n<2){
-		int startA, endA, startB, endB;
-		coranks(start, A, lenA, &startA, B, lenB, &startB);
-		coranks(end, A, lenA, &endA, B, lenB, &endB);
-		(void) sequential_merge(A+startA, endA-startA, B+startB, endB-startB, result+start);
-	}
-	else{
-		//distribute the work across threads
-		int middle = start + (end-start)/2;
-		#pragma omp parallel sections 
-		{
-
-			#pragma omp section
-			omp_merge_part(start, middle, A, lenA, B, lenB, result);
-
-			#pragma omp section
-			omp_merge_part(middle, end, A, lenA, B, lenB, result);
-
-		}
-		
-	}
 }
