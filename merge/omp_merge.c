909d31ba0de702413ee60b9d50cc1ed9c022d29f
enrimilan <enrimiho@gmail.com>
2015-12-22 22:00:17 +0100

    use long type to prevent overflows.

--- merge/omp_merge.c
+++ merge/omp_merge.c
@@ -8,14 +8,14 @@ double omp_merge(data_t *A, int lenA, data_t *B, int lenB, data_t *result) {
 
 	//usually some multiple of the number of cores	
 	int p = omp_get_max_threads();
-	double start = getTimestamp();
+	double start1 = getTimestamp();
 	#pragma omp parallel
 	{
  		#pragma omp for
  		for (int i=0; i<p; i++) {
  			int startA, endA, startB, endB;
- 			int start = i*(lenA+lenB)/p;
- 			int end = (i+1)*(lenA+lenB)/p;
+ 			long start = (long)i*(long)(lenA+lenB)/(long)p;
+ 			long end = (long)(i+1)*(long)(lenA+lenB)/(long)p;
 			coranks(start, A, lenA, &startA, B, lenB, &startB);
 			coranks(end, A, lenA, &endA, B, lenB, &endB);
 			(void) sequential_merge(A+startA, endA-startA, B+startB, endB-startB, result+start);
@@ -23,5 +23,5 @@ double omp_merge(data_t *A, int lenA, data_t *B, int lenB, data_t *result) {
 	}
 	double end = getTimestamp();
 	
-	return end-start;
+	return end-start1;
 }
