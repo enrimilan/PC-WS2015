ddbc6af12712533aec7ed151b04b8cd1f7a8d12e
Stefan Karner <e1027209@student.tuwien.ac.at>
2016-01-29 10:51:28 +0100

    Added a shell script for building documentation

---
+++ doc/build.sh
@@ -0,0 +1,20 @@
+#!/bin/sh
+
+SOURCE=doc			# Document name
+OUT_DIR=output		# LaTeX output folder
+
+
+# Create output folder if it does not exist
+mkdir -p $OUT_DIR
+OPTS=-output-directory=$OUT_DIR
+PL_ARGS=$OPTS" "$SOURCE
+
+# Build document
+pdflatex $PL_ARGS
+bibtex   $OUT_DIR/$SOURCE
+pdflatex $PL_ARGS
+pdflatex $PL_ARGS
+
+# Move pdf to current folder
+mv $OUT_DIR/$SOURCE.pdf $SOURCE.pdf
+echo -e "\n\nThesis document compiled."
